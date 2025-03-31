19d0f8003181cde9720c915e631cd4d833e555b6
Pietroo03 <pietro.ponte.03@gmail.com>
2025-03-31 15:17:07 +0200

    Initial commit

---
+++ demo/src/main/java/org/project/spring/music_album/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package org.project.spring.music_album.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
