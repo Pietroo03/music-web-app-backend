ed25fa1d00e339b01a1939b6a2b79a68d67475ca
Pietroo03 <pietro.ponte.03@gmail.com>
2025-03-31 19:15:34 +0200

    add controller for artists and albums

---
+++ demo/src/main/java/org/project/spring/music_album/demo/controllers/AlbumRestController.java
@@ -0,0 +1,74 @@
+package org.project.spring.music_album.demo.controllers;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.project.spring.music_album.demo.model.Album;
+import org.project.spring.music_album.demo.service.AlbumService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/albums")
+public class AlbumRestController {
+
+    @Autowired
+    private AlbumService albumService;
+
+    @GetMapping
+    public List<Album> index() {
+        List<Album> artisti = albumService.findAll();
+        return artisti;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Album> show(@PathVariable Integer id) {
+        Optional<Album> albumAttempt = albumService.findById(id);
+
+        if (albumAttempt.isEmpty()) {
+            return new ResponseEntity<Album>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<Album>(albumAttempt.get(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public Album store(@Valid @RequestBody Album album) {
+        return albumService.create(album);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Album> update(@Valid @RequestBody Album album, @PathVariable Integer id) {
+
+        if (albumService.findById(id).isEmpty()) {
+            return new ResponseEntity<Album>(HttpStatus.NOT_FOUND);
+        }
+
+        album.setId(id);
+        return new ResponseEntity<Album>(albumService.update(album), HttpStatus.OK);
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Album> delete(@Valid @PathVariable Integer id) {
+
+        if (albumService.findById(id).isEmpty()) {
+            return new ResponseEntity<Album>(HttpStatus.NOT_FOUND);
+        }
+
+        albumService.deleteById(id);
+        return new ResponseEntity<Album>(HttpStatus.OK);
+
+    }
+}
---
+++ demo/src/main/java/org/project/spring/music_album/demo/controllers/ArtistaRestController.java
@@ -0,0 +1,74 @@
+package org.project.spring.music_album.demo.controllers;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.project.spring.music_album.demo.model.Artista;
+import org.project.spring.music_album.demo.service.ArtistaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/artists")
+public class ArtistaRestController {
+
+    @Autowired
+    private ArtistaService artistaService;
+
+    @GetMapping
+    public List<Artista> index() {
+        List<Artista> artisti = artistaService.findAll();
+        return artisti;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Artista> show(@PathVariable Integer id) {
+        Optional<Artista> artistaAttempt = artistaService.findById(id);
+
+        if (artistaAttempt.isEmpty()) {
+            return new ResponseEntity<Artista>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<Artista>(artistaAttempt.get(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public Artista store(@Valid @RequestBody Artista artista) {
+        return artistaService.create(artista);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Artista> update(@Valid @RequestBody Artista artista, @PathVariable Integer id) {
+
+        if (artistaService.findById(id).isEmpty()) {
+            return new ResponseEntity<Artista>(HttpStatus.NOT_FOUND);
+        }
+
+        artista.setId(id);
+        return new ResponseEntity<Artista>(artistaService.update(artista), HttpStatus.OK);
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Artista> delete(@Valid @PathVariable Integer id) {
+
+        if (artistaService.findById(id).isEmpty()) {
+            return new ResponseEntity<Artista>(HttpStatus.NOT_FOUND);
+        }
+
+        artistaService.deleteById(id);
+        return new ResponseEntity<Artista>(HttpStatus.OK);
+
+    }
+}
